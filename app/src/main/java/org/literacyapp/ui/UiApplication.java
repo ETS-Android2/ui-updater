aadb3719679f62a0a70a0006bb158914c58e1a3d
Jo Grimstad <jo@literacyapp.org>
2016-11-27 15:32:38 +0300

    pref_app_version_code

--- app/src/main/java/org/literacyapp/ui/UiApplication.java
+++ app/src/main/java/org/literacyapp/ui/UiApplication.java
@@ -9,7 +9,7 @@ import org.literacyapp.ui.util.VersionHelper;
 
 public class UiApplication extends Application {
 
-    public static final String PREF_APP_VERSION_CODE = "pref_last_content_sync";
+    public static final String PREF_APP_VERSION_CODE = "pref_app_version_code";
 
     @Override
     public void onCreate() {
