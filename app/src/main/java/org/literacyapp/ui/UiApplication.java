2740cb8673642ccd45822377f8f81f81418a998f
Jo Grimstad <jo@literacyapp.org>
2016-11-27 13:53:22 +0300

    Add versionCode upgrade detection

--- app/src/main/java/org/literacyapp/ui/UiApplication.java
+++ app/src/main/java/org/literacyapp/ui/UiApplication.java
@@ -1,8 +1,31 @@
 package org.literacyapp.ui;
 
-/**
- * Created by jogrimst on 27/11/16.
- */
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
-public class UiApplication {
+import org.literacyapp.ui.util.VersionHelper;
+
+public class UiApplication extends Application {
+
+    public static final String PREF_APP_VERSION_CODE = "pref_last_content_sync";
+
+    @Override
+    public void onCreate() {
+        Log.i(getClass().getName(), "onCreate");
+        super.onCreate();
+
+        // Check if the application's versionCode was upgraded
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        int oldVersionCode = sharedPreferences.getInt(PREF_APP_VERSION_CODE, 0);
+        int newVersionCode = VersionHelper.getAppVersionCode(getApplicationContext());
+        if ((oldVersionCode > 0) && (oldVersionCode < newVersionCode)) {
+            Log.i(getClass().getName(), "Upgrading application from version " + oldVersionCode + " to " + newVersionCode);
+//            if (newVersionCode == ???) {
+//                // Put relevant tasks required for upgrading here
+//            }
+            sharedPreferences.edit().putInt(PREF_APP_VERSION_CODE, newVersionCode).commit();
+        }
+    }
 }
